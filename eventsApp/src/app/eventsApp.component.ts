6470dd43485430766055d03c0dcb52217dc919a3
Mark Vaykhansky <prhing@gmail.com>
2018-12-01 08:15:12 +0200

    Rename root-component to events-app-component Add an image to the main page

---
+++ eventsApp/src/app/eventsApp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h2> hello wrold </h2><img src="/assets/images/ng-nl.png"/>',
+  styleUrls: ['./app.component.css']
+})
+export class EventsAppComponent {
+  title = 'Events App';
+}
